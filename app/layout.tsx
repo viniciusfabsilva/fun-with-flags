c1cbffa4841398de1ee7c8e40b2c329183143440
vinicius <viniciusfabi1@hotmail.com>
2025-02-13 23:27:16 -0300

    restore layout

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./globals.css";
+
+const noto_Sans = Noto_Sans({
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Fun with flags",
+  description: "Flags of the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${noto_Sans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
