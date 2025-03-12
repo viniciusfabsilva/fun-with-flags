7baec976e91851957043e310fe38c417ff7db002
vinicius <viniciusfabi1@hotmail.com>
2025-03-11 23:30:24 -0300

    get countries data from rest countries API

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,21 +1,29 @@
+import Image from "next/image";
+
 type CardProps = {
-  country: string;
+  index: number;
+  flag: string;
+  name: string;
   capital: string;
   region: string;
-  population: string;
+  population: number;
 };
 
-const Card = ({ country, capital, region, population }: CardProps) => {
+const Card = ({ index, flag, name, capital, region, population }: CardProps) => {
   return (
     <div className="h-full bg-white overflow-hidden rounded-lg shadow-lg">
       <div className="aspect-video w-full">
-        <img
+        <Image
+          alt={`Flag of ${name}`}
           className="w-full h-full object-cover"
-          src="https://placehold.co/600x400"
+          src={flag || "https://via.placeholder.com/300"}
+          width={500}
+          height={300}
+          priority={index < 12}
         />
       </div>
       <div className="p-6 text-sm text-gray-600">
-        <h2 className="text-xl font-semibold mb-4">{country}</h2>
+        <h2 className="text-xl font-semibold mb-4">{name}</h2>
         <div className="space-y-2">
           <div className="flex items-center gap-1">
             <span className="font-semibold">Capital:</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,72 @@
-import { Header, Card, Footer, Grid } from "./components";
+"use client";
 
-const countries = [
-  {
-    id: 1,
-    country: "Brazil",
-    capital: "Brasília",
-    region: "América do Sul",
-    population: "211000000",
-  },
-  {
-    id: 2,
-    country: "Japan",
-    capital: "Tokyo",
-    region: "Ásia",
-    population: "126000000",
-  },
-  {
-    id: 3,
-    country: "United States",
-    capital: "Washington D.C.",
-    region: "América do Norte",
-    population: "331000000",
-  },
-  {
-    id: 4,
-    country: "Germany",
-    capital: "Berlin",
-    region: "Europa",
-    population: "83000000",
-  },
-  {
-    id: 5,
-    country: "India",
-    capital: "New Delhi",
-    region: "Ásia",
-    population: "1380000000",
-  },
-  {
-    id: 6,
-    country: "Australia",
-    capital: "Canberra",
-    region: "Oceania",
-    population: "25000000",
-  },
-  {
-    id: 7,
-    country: "Nigeria",
-    capital: "Abuja",
-    region: "África",
-    population: "200000000",
-  },
-];
+import { Header, Card, Footer, Grid } from "./components";
+import { useEffect, useState } from "react";
+
+type Country = {
+  cca3: string;
+  name: {
+    common: string;
+  };
+  flags: {
+    svg: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+};
 
 export default function Home() {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(
+          "https://restcountries.com/v3.1/all?fields=cca3,name,capital,region,population,flags"
+        );
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        setError("Failed to fetch data");
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
   return (
     <>
       <Header />
       <main className="flex-1">
         <Grid>
-          {countries.map(({ id, country, capital, region, population }) => (
-            <Card
-              key={id}
-              country={country}
-              capital={capital}
-              region={region}
-              population={population}
-            />
-          ))}
+          {countries.map(
+            ({ cca3, flags, name, capital, region, population }, index) => {
+              const { svg: flag } = flags ?? {};
+              const { common: countryName } = name ?? {};
+              const [capitalName] = capital ?? [];
+
+              return (
+                <Card
+                  index={index}
+                  key={cca3}
+                  name={countryName}
+                  flag={flag}
+                  capital={capitalName}
+                  region={region}
+                  population={population}
+                />
+              );
+            }
+          )}
         </Grid>
       </main>
       <Footer />
