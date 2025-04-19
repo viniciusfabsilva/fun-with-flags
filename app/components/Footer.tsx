80aa6f1949132ec951f0e3aaedb3288809907dc7
vinicius <viniciusfabi1@hotmail.com>
2025-04-18 23:16:24 -0300

    adjust footer margin

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-6 mb-8">
+    <footer className="py-6 mt-8">
       <p className="text-center">Make by Vinicius Silva</p>
     </footer>
   );
