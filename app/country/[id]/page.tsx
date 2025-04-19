4ab4644fc91fa13068377866eedaeff1dd257af9
vinicius <viniciusfabi1@hotmail.com>
2025-04-19 19:27:15 -0300

    map detailed country data to the page

--- app/country/[id]/page.tsx
+++ app/country/[id]/page.tsx
@@ -6,15 +6,31 @@ import { useEffect, useState } from "react";
 import { countriesApi } from "../../services";
 import { useParams } from "next/navigation";
 
+type DetailedCountry = {
+  cca3: string;
+  name: {
+    common: string;
+  };
+  flags: {
+    svg: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+  languages: Record<string, string>;
+  currencies: Record<string, { name: string; symbol: string }>;
+  tld: string[];
+  borders: string[];
+};
+
 type Params = {
   id: string;
 };
 
 export default function Country() {
-  const name = "Brazil";
   const params = useParams<Params>();
 
-  const [country, setCountry] = useState<Country>();
+  const [country, setCountry] = useState<DetailedCountry>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [id, setId] = useState<string | null>(null);
@@ -47,6 +63,29 @@ export default function Country() {
   if (error) return <div>{error}</div>;
 
   console.log(country);
+  const {
+    flags,
+    capital,
+    name,
+    region,
+    population,
+    languages,
+    currencies,
+    tld,
+    borders,
+  } = country ?? {};
+
+  const { svg: flag } = flags ?? {};
+  const [capitalName] = capital ?? [];
+  const { common: countryName } = name ?? {};
+
+  const languagesNames = Object.values(languages ?? {}).join(", ");
+  const currenciesNames = Object.values(currencies ?? {}).map(
+    ({ name, symbol }) => `${name} (${symbol})`
+  );
+  const [topLevelDomain] = tld ?? [];
+  const bordersIds = borders?.join(", ") ?? "";
+
 
   return (
     <>
@@ -58,45 +97,46 @@ export default function Country() {
         </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] gap-4">
-        <div className="w-full md:max-w-[400px]">
+        <div className="flex items-center md:max-w-[400px]">
           <Image
             alt={`Flag of ${name}`}
-            className="w-full h-full"
-            src={"/flag-placeholder.svg"}
+            className="max-h-80 object-cover rounded-lg shadow-md"
+            src={flag || "/flag-placeholder.svg"}
             width={500}
             height={300}
+            priority
           />
         </div>
         <div className="flex flex-col justify-center p-6 text-sm text-gray-600">
-          <h2 className="text-xl font-semibold mb-4">Brazil ({id})</h2>
+          <h2 className="text-xl font-semibold mb-4">{ countryName } ({id})</h2>
           <div className="space-y-2">
             <div className="flex items-center gap-1">
               <span className="font-semibold">Capital:</span>
-              <span>Brasilia</span>
+              <span>{capitalName}</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">region:</span>
-              <span>South America</span>
+              <span>{ region }</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">Population:</span>
-              <span>212559409</span>
+              <span>{ population }</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">Languages:</span>
-              <span>Portuguese</span>
+              <span>{ languagesNames }</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">Currencies:</span>
-              <span>BRL</span>
+              <span>{ currenciesNames }</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">Top Level Domain:</span>
-              <span>.br</span>
+              <span>{topLevelDomain}</span>
             </div>
             <div className="flex items-center gap-1">
               <span className="font-semibold">Borders:</span>
-              <span>ARG, BOL, COL, GUF, GUY, PRY, PER, SUR, URY, VEN</span>
+              <span>{ bordersIds }</span>
             </div>
           </div>
         </div>
