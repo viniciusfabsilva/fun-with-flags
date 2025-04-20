52cc441ae4c7cd6c0eddcd660c32fa9e6460f36a
vinicius <viniciusfabi1@hotmail.com>
2025-04-20 00:20:13 -0300

    refacotr select to use a custom one

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -1,4 +1,6 @@
-// import { ChevronDownIcon } from "@heroicons/react/24/outline";
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 type SelectProps = {
   options: string[];
@@ -7,24 +9,111 @@ type SelectProps = {
 };
 
 const Select = ({ options, selected, setSelected }: SelectProps) => {
-  const selectOptions = ["All Regions", ...options];
+  const [isOpen, setIsOpen] = useState(false);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      listRef.current?.focus();
+      setFocusedIndex(0);
+    } else {
+      setFocusedIndex(-1);
+    }
+  }, [isOpen]);
+
+  const handleButtonKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter" || event.key === "Space") {
+      event.preventDefault();
+      setIsOpen(true);
+    }
+  };
+
+  const handleListKeyDown = (event: React.KeyboardEvent) => {
+    switch (event.code) {
+      case "ArrowDown":
+        event.preventDefault();
+        setFocusedIndex((prevIndex) =>
+          prevIndex < options.length - 1 ? prevIndex + 1 : 0
+        );
+        break;
+
+      case "ArrowUp":
+        event.preventDefault();
+        setFocusedIndex((prevIndex) =>
+          prevIndex > 0 ? prevIndex - 1 : options.length - 1
+        );
+        break;
+
+      case "Enter":
+      case "Space": {
+        const selectedOption = options[focusedIndex];
+        setSelected(selectedOption);
+        setIsOpen(false);
+        break;
+      }
+
+      case "Escape":
+        event.preventDefault();
+        setIsOpen(false);
+        break;
+
+      case "Tab":
+        event.preventDefault();
+        break;
+    }
+  };
 
   return (
-    <div className="w-1/3">
-      <select
-        value={selected}
-        className="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none border-gray-300"
-        onChange={({ target }) => setSelected(target.value)}
+    <div className="w-1/3 relative">
+      <button
+        type="button"
+        aria-haspopup="listbox"
+        aria-expanded={isOpen}
+        onKeyDown={handleButtonKeyDown}
+        aria-labelledby="listbox"
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full flex justify-between items-center px-4 py-2 
+      text-left border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 
+      focus:outline-none border-gray-300"
       >
-        {selectOptions.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-      {/* <span className="">
-        <ChevronDownIcon className="size-4" />
-      </span> */}
+        {selected}
+        <ChevronDownIcon
+          className={`size-4 ${isOpen ? "transform rotate-180" : ""}`}
+        />
+      </button>
+
+      {isOpen && (
+        <ul
+          ref={listRef}
+          id="listbox"
+          role="listbox"
+          tabIndex={0}
+          onKeyDown={handleListKeyDown}
+          aria-activedescendant={`option-${focusedIndex}`}
+          className="absolute bg-white mt-2 border rounded-lg 
+        border-gray-300 shadow-md w-full overflow-hidden focus:ring-2 
+        focus:ring-blue-500 focus:outline-none"
+        >
+          {options.map((option, index) => (
+            <li
+              key={option}
+              id={`option-${index}`}
+              role="option"
+              aria-selected={option === selected}
+              className={`px-4 py-2 text-left hover:bg-gray-100 cursor-pointer ${
+                focusedIndex === index ? "bg-blue-100" : "hover:bg-gray-100"
+              }`}
+              onClick={() => {
+                setSelected(option);
+                setIsOpen(false);
+              }}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -43,8 +43,7 @@ export default function Home() {
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
-
-  const regions = [...new Set(countries.map(({ region }) => region))];
+  const regions = ["All Regions", ...new Set(countries.map(({ region }) => region))];
 
   const sortedCountries = countries.sort((a, b) =>
     a.name.common.localeCompare(b.name.common, "en-US")
